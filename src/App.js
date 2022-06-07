0e0ced39b81b9554317d8622af38835e0d50ec14
cmacritchie <craig.macritchie@gmail.com>
2022-06-07 11:50:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import M from 'materialize-css'
+import 'materialize-css/dist/css/materialize.min.css'
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div class="card-panel teal lighten-2">This is a card panel with a teal lighten-2 class</div>
+        <a class="waves-effect waves-light btn"><i class="material-icons cloud left">cloud</i>button</a>
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
